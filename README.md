3fd4f9aebaf223bf85337d1dd75886e3a4f2ca88
GrayCoded <130934177+GrayCoded@users.noreply.github.com>
2023-05-29 17:56:21 -0300

    Wrote a majority of the README file. still need to add the screenshots and find out why my html page isnt working.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # JavaScriptQuiz
-Through the use of html/css/javascript and utilizing local storage to log and calculate scores.
+
+## Description
+
+- This is a quick build for a JavaScript multiple-choice     question quiz where the time it takes you to finish the quiz is your overall score. 
+- This will be stored on the local system which will allow you to be able to continually retry and challenge to get a higher score. 
+- This application will allow seemless transition between the question pages/HIGH SCORE ect.
+- This application utilizes HTML/CSS/JavaScript.
+
+Table of Contents
+
+1. Description - JavaScriptQuiz - GreyCoded
+2. Installation - Website 
+3. Usage - Step by Step process for the game
+4. Credit - 
+5. License - MIT - 
+6.
+
+## Installation 
+
+1. Go to website ==> 
+2. Once on site follow instructions as provided on page.
+
+## Usage.
+
+1. Once the site is activated you will be prompted to the following main page.
+
+2. Here you will be prompted to start the quiz and be given instructions on how it works and how you will be scored. 
+
+3. There will also be two elements (HIGH SCORE/Time) which each has its own attributes. 
+
+4. The High Score link will take the user to a div element where a list of previous users will be listed. 
+
+5. The Time element will show the current time and will subsequently be how you are scored throughout the quiz.
+
+6. Once the user has clicked the "start" button the quiz will start.
+
+7. The page will change to a series of question and answer pages that upon selection of an answer with determine how much time will be taken off the clock. (wrong=-15seconds)(right=-5seconds.)
+
+8. Once either the questions have all been answered or the user has run out of time they will be taken to the finish page.
+
+9. Once there they will shown their score and prompted to enter in their initials.
+
+10. Once completed and the user has clicked the submit. The user information is stored locally and used to populate the list on the high Score page.
+
+## Credit
+
+## License
+
+- Refer to repo. ==>
--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,168 @@
-var usernameL = 3;
-var choice = [];
-
-var timerEL = document.querySelector('#timer')
-var questionEl = document.querySelector('#question')
-var answerEl = document.querySelector('#answer')
-var highScoreEl = document.querySelector('#high-score')
-var initialsEl = document.querySelector('#Initials')
-var titleEl = document.querySelector('#h1')
-
-
-var questionStr = [ {
-
+var questionStr = [ 
+    {
     question:'JavaScript is a ________ language?',
-    answers:('Data','Love','Binary','simple'),
-    correctAnswer:'Data',
-},{
+    answers:['Data','Love','Binary','simple'],
+    correctA:'Data',
+    },
+    {
    question: 'How do you log data to the page?',
-   answers: ('console.log','write','paste','put'),
-   correctAnswer: 'console.log',
+   answers: ['console.log','write','paste','put'],
+   correctA: 'console.log',
 
-},{
+    },
+    {
     question: 'Which is a Primitive Data Type?',
-    answers: ('Boolean','Byte','Quibit','Dongle'),
-    correctAnswer: 'Boolean',
-},{
+    answers: ['Boolean','Byte','Quibit','Dongle'],
+    correctA: 'Boolean',
+    },
+    {
     question: 'Can store an object as a string?',
-    answers: ('Yes','No'),
-    correctAnswer: 'Yes',
-},{
+    answers: ['Yes','No'],
+    correctA: 'Yes',
+    },
+    {
     question: 'What is the purpose of JavaScript?',
-    answers: ('To give dynamic change to a website','Provides the base', 'Acts as a style','A type of Grater'),
-    correctAnswer: 'To give dynamic change to a website',
-},{
+    answers: ['To give dynamic change to a website','Provides the base', 'Acts as a style','A type of Grater'],
+    correctA: 'To give dynamic change to a website',
+    },
+    {
     question: 'What can you store in an Array?',
-    answers: ('Numbers','Data','Strings','All of the Above'),
-    correctAnswer: 'All of the Above',
-}   
-]
+    answers: ['Numbers','Data','Strings','All of the Above'],
+    correctA: 'All of the Above',
+    },   
+];
 
-function generateQuestion() {
-    var questionStr =('');
-    for(var i =0; i < questionStr) 
-    var randomizer = Math.floor(Math.random() * questions.length);
-    questionStr = questionStr + choice[randomizer]
-    return questionStr;
 
-}
+var score = 0;
+var questions = 0;
+var currentTime = document.querySelector('#currentTime');
+var timer = document.querySelector('#questionsDiv');
+var wrapper = document.querySelector('#wrapper');
 
 
 
 var timeLeft = 100;
+var holdInterval = 0;
+var penaltyC = 5;
+var penaltyW = 15;
+var ulCreate = document.createElement("ul");
 
-function setTime() {
-    var timeInterval = setInetravl(function() {
-        timeLeft--;
-        timeLeft.textContent = 'Time: ' + timeLeft;
+timer.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            timeLeft--;
+            currentTime.textContext = "Time: " + timeLeft;
+        
 
-        if(timeLeft === 0) {
-            clearInterval(timeInterval);
-            prompt("You have Run out of Time");
+        if (timeLeft <= 0) {
+            clearInterval(holdInterval);
+            finish();
+            currentTime.textcontext = "You have Run out of Time!"
         }
+        }, 1000);
+        } 
+        generateQuestions(questions);
+    })
 
-    }, 1000);
+
+
+function generateQuestions(questions) {
+    questionsDiv.container = '';
+    ulCreate.container = '';
+    for(var i = 0; i < questionsStr.length; i++) {
+        var userQ = questionStr[questions].question;
+        var userA = questionStr[questions].answers;
+        questionsDiv.textContext = userQ;
+    }
+    userA.forEach(function (newItem) {
+        listItem.textContext = newItem;
+        questionsDiv.appendChild(ulCreate);
+        ulCreate.apphendChild(listItem);
+        listItem.addEventListener("click", validate)
+    });
 }
 
+function validate(event) {
+    var selectedA = event.target;
+    if (selectedA.matches('li')) {
+        var createDiv = document.createElement('div')
+        createDiv.setAttribute('id',"createDiv");
+        if (selectedA.textContent == questionStr[questions].correctA) {
+            createDiv.textContext = "Correct!";
+            timeLeft = timeLeft - penaltyC;
+    }   else {
+            timeLeft = timeLeft - penaltyW;
+            createDiv.textContext = 'Wrong!';
 
-setTime();
+    }
+}
+    
+questions++;
+
+var finishDiv = document.querySelector('#quizfinish')
+
+if (questions >= questionStr.length) {
+    finish();
+    createDiv.textContent = "Congratulations!" + "";
+} else {
+    generateQuestions(questions);
+}
+questionDiv.appendChild(createDiv);
+}
+
+function finish() {
+    questionsDiv.hide = '';
+    currentTime.hide = '';
+
+    var createH1 = document.createElement('h1');
+    createH1.setAttribute('id','createH1');
+    createH1.textContent = "Finished!";
+
+    questionsDiv.appendchild(createH1);
+
+    if (timeLeft >= 0) {
+        var remainingT = timeLeft;
+        var createP = document.createElement('p');
+        clearInterval(holdInterval);
+        createP.textContext = "Score: " + remainingT;
+
+        questionsDiv.appendChild(createP);
+    }
+
+var highScores = document.querySelector('#highScores')
+var back = document.querySelector('#return')
+var wipe = document.querySelector('#wipe')
+
+    wipe.addEventListener('click', function () {
+        localStorage.wipe();
+        location.reload();
+    });
+    var 
+
+
+var submitBtn = document.querySelector('#submit')
+
+    submitBtn.addEventListener('click',function () {
+    var initials = createInput.value;
+
+    if (initials === null) {
+        alert('No Value Entered');
+    }   else {
+        var finalScore = {
+            initails: initials,
+            score: remainingT,
+        };
+        console.log(finalScore);
+        var highScores = local.Storage.getItem("highScores");
+        if (highScores = null) {
+            highScores = [];
+        } else {
+            highScores = JSON.parse("highScores");
+        }
+        highScores.push(finalScore);
+        var nScore = JSON.stringify(highScores);
+        localStorage.setItem("highScores",nscore);
+        window.location.replace("highScoreDiv");
+    }
+});
+}
